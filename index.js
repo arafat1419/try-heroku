ddd1bf75edb1da303a28c5e698b64d4c8c062801
Mohammad Arafat Maku <arafat1419@gmail.com>
2022-08-31 20:12:18 +0800

    port

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 app.get('/', (req, res) => res.send('Hello Express!'))
 app.get('/about', (req, res) => res.send('Express is Express'))
 
-app.listen(3000, () => console.log('Hello World app berjalan di http://localhost:3000'))
+app.listen(8080, () => console.log('Hello World app berjalan di http://localhost:3000'))
